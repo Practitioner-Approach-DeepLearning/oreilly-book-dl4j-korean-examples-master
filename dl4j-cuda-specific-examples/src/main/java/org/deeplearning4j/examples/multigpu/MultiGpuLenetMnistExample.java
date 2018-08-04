f4d9b519e4c67a8a87ef8cd439b31866046dc825
greentbag <audiosignal@gmail.com>
2018-08-04 20:56:08 +0900

    dl4j-cuda-specific-examples 주석 번역 완료

--- dl4j-cuda-specific-examples/src/main/java/org/deeplearning4j/examples/multigpu/MultiGpuLenetMnistExample.java
+++ dl4j-cuda-specific-examples/src/main/java/org/deeplearning4j/examples/multigpu/MultiGpuLenetMnistExample.java
@@ -29,7 +29,7 @@ import org.slf4j.LoggerFactory;
 
 
 /**
- * This is modified version of original LenetMnistExample, made compatible with multi-gpu environment
+ * 이 예제는 LenetMnistExample의 수정 버전으로 다중 GPU 환경과 호환된다.
  *
  * @author  @agibsonccc
  * @author raver119@gmail.com
@@ -38,26 +38,26 @@ public class MultiGpuLenetMnistExample {
     private static final Logger log = LoggerFactory.getLogger(MultiGpuLenetMnistExample.class);
 
     public static void main(String[] args) throws Exception {
-        // PLEASE NOTE: For CUDA FP16 precision support is available
+        // 필독: CUDA FP 16 정밀도를 지원한다.
         DataTypeUtil.setDTypeForContext(DataBuffer.Type.HALF);
 
-        // temp workaround for backend initialization
+        // 백엔드 초기화를 할 수 있는 임시적인 방법
         Nd4j.create(1);
 
         CudaEnvironment.getInstance().getConfiguration()
-            // key option enabled
+            // 키 옵션 사용
             .allowMultiGPU(true)
 
-            // we're allowing larger memory caches
+            // 메모리 캐시 증가
             .setMaximumDeviceCache(2L * 1024L * 1024L * 1024L)
 
-            // cross-device access is used for faster model averaging over pcie
+            // 장치 간 직접 접근 기능을 사옹하면 PCIe를 거쳐갈 때보다 모델 평균화가 빠르다.
             .allowCrossDeviceAccess(true);
 
         int nChannels = 1;
         int outputNum = 10;
 
-        // for GPU you usually want to have higher batchSize
+        // GPU를 사용할 때는 배치 크기를 더 크게 잡으면 좋다.
         int batchSize = 128;
         int nEpochs = 10;
         int iterations = 1;
@@ -70,10 +70,10 @@ public class MultiGpuLenetMnistExample {
         log.info("Build model....");
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
             .seed(seed)
-            .iterations(iterations) // Training iterations as above
+            .iterations(iterations) // 학습 반복 횟수 설정
             .regularization(true).l2(0.0005)
                 /*
-                    Uncomment the following for learning decay and bias
+                    학습 감쇠 및 편향을 적용하려면 아래 주석을 해제할 것
                  */
             .learningRate(.01)//.biasLearningRate(0.02)
             //.learningRateDecayPolicy(LearningRatePolicy.Inverse).lrPolicyDecayRate(0.001).lrPolicyPower(0.75)
@@ -82,7 +82,7 @@ public class MultiGpuLenetMnistExample {
             .updater(Updater.NESTEROVS).momentum(0.9)
             .list()
             .layer(0, new ConvolutionLayer.Builder(5, 5)
-                //nIn and nOut specify depth. nIn here is the nChannels and nOut is the number of filters to be applied
+                // nIn과 nOut으로 깊이 지정. nIn은 채널 수고 nOut은 필터 수다.
                 .nIn(nChannels)
                 .stride(1, 1)
                 .nOut(20)
@@ -93,7 +93,7 @@ public class MultiGpuLenetMnistExample {
                 .stride(2,2)
                 .build())
             .layer(2, new ConvolutionLayer.Builder(5, 5)
-                //Note that nIn need not be specified in later layers
+                // 이 계층부터는 nIn을 설정할 필요가 없음
                 .stride(1, 1)
                 .nOut(50)
                 .activation(Activation.IDENTITY)
@@ -108,26 +108,26 @@ public class MultiGpuLenetMnistExample {
                 .nOut(outputNum)
                 .activation(Activation.SOFTMAX)
                 .build())
-            .setInputType(InputType.convolutionalFlat(28,28,1)) //See note below
+            .setInputType(InputType.convolutionalFlat(28,28,1)) // 아래 주석 참조
             .backprop(true).pretrain(false).build();
         MultiLayerNetwork model = new MultiLayerNetwork(conf);
         model.init();
 
-        // ParallelWrapper will take care of load balancing between GPUs.
+        // ParallelWrapper는 GPU 간 로드밸런싱을 처리함
         ParallelWrapper wrapper = new ParallelWrapper.Builder(model)
-            // DataSets prefetching options. Set this value with respect to number of actual devices
+            // DataSets 선적재 옵션. 실제 장치 개수에 따라 값을 설정
             .prefetchBuffer(24)
 
-            // set number of workers equal or higher then number of available devices. x1-x2 are good values to start with
+            // 사용 가능한 장치 개수 대비 같거나 많게 설정. x1-x2로 시작하면 좋다.
             .workers(4)
 
-            // rare averaging improves performance, but might reduce model accuracy
+            // 평균화 빈도가 줄어들면 성능은 향상되지만 모델 정확도는 감소할 수 있음
             .averagingFrequency(3)
 
-            // if set to TRUE, on every averaging model score will be reported
+            // true로 설정하면 모든 평균 모델 점수가 보고됨
             .reportScoreAfterAveraging(true)
 
-            // optinal parameter, set to false ONLY if your system has support P2P memory access across PCIe (hint: AWS do not support P2P)
+            // 선택 옵션, 시스템이 PCIe에서 P2P 메모리 액세스를 지원하는 경우에만 false로 설정(참고: AWS는 P2P를 지원하지 않음)
             .useLegacyAveraging(true)
 
             .build();
@@ -136,13 +136,13 @@ public class MultiGpuLenetMnistExample {
         model.setListeners(new ScoreIterationListener(100));
         long timeX = System.currentTimeMillis();
 
-        // optionally you might want to use MultipleEpochsIterator instead of manually iterating/resetting over your iterator
+        // 반복자를 수동으로 반복/재설정하는 대신 MultipleEpochsIterator를 사용할 수도 있음
         //MultipleEpochsIterator mnistMultiEpochIterator = new MultipleEpochsIterator(nEpochs, mnistTrain);
 
         for( int i=0; i<nEpochs; i++ ) {
             long time1 = System.currentTimeMillis();
 
-            // Please note: we're feeding ParallelWrapper with iterator, not model directly
+            // 필독: ParallelWrapper에 모델을 직접 전달하지 않고 iterator를 전달할 수도 있음
 //            wrapper.fit(mnistMultiEpochIterator);
             wrapper.fit(mnistTrain);
             long time2 = System.currentTimeMillis();
