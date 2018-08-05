834fd63fafdd8ca3b1c7d69895002d1190b62b1c
DaleePark <39670191+DaleePark@users.noreply.github.com>
2018-08-05 15:55:29 +0900

    Update AsyncNStepCartpole.java
    convert_kor

--- rl4j-examples/src/main/java/org/deeplearning4j/examples/rl4j/AsyncNStepCartpole.java
+++ rl4j-examples/src/main/java/org/deeplearning4j/examples/rl4j/AsyncNStepCartpole.java
@@ -12,33 +12,33 @@ import org.deeplearning4j.rl4j.util.DataManager;
 /**
  * @author rubenfiszel (ruben.fiszel@epfl.ch) on 8/18/16.
  *
- * main example for Async NStep QLearning on cartpole
+ * cartpole 문제에서 비동기 NStep Q러닝 실행 예제 
  */
 public class AsyncNStepCartpole {
 
 
     public static AsyncNStepQLearningDiscrete.AsyncNStepQLConfiguration CARTPOLE_NSTEP =
             new AsyncNStepQLearningDiscrete.AsyncNStepQLConfiguration(
-                    123,     //Random seed
-                    200,     //Max step By epoch
-                    300000,  //Max step
-                    16,      //Number of threads
+                    123,     //랜덤 시드
+                    200,     //에포크 최대 스텝 
+                    300000,  //최대 스텝 
+                    16,      //쓰레드 수
                     5,       //t_max
-                    100,     //target update (hard)
-                    10,      //num step noop warmup
-                    0.01,    //reward scaling
+                    100,     //타겟 업데이트(hard)
+                    10,      //noop 시작 스텝 수
+                    0.01,    //보상 스케일링
                     0.99,    //gamma
-                    100.0,   //td-error clipping
-                    0.1f,    //min epsilon
-                    9000     //num step for eps greedy anneal
+                    100.0,   //td-error 클립핑
+                    0.1f,    //최소 입실론
+                    9000     //입실론 탐욕 담금질(eps greedy anneal)을 위한 스텝 수
             );
 
     public static DQNFactoryStdDense.Configuration CARTPOLE_NET_NSTEP =
             new DQNFactoryStdDense.Configuration(
-                    3,         //number of layers
-                    16,        //number of hidden nodes
-                    0.0005,    //learning rate
-                    0.001      //l2 regularization
+                    3,         //계층 수
+                    16,        //은닉 노드 수
+                    0.0005,    //학습률
+                    0.001      //l2 정규화
             );
 
 
@@ -50,10 +50,10 @@ public class AsyncNStepCartpole {
 
     public static void cartPole() {
 
-        //record the training data in rl4j-data in a new folder
+        //새 폴더의 rl4j-data에 학습 데이터 기록
         DataManager manager = new DataManager(true);
 
-        //define the mdp from gym (name, render)
+        //gym (name, render)을 이용해서 mdp 정의 
         GymEnv mdp = null;
         try {
         mdp = new GymEnv("CartPole-v0", false, false);
@@ -61,13 +61,13 @@ public class AsyncNStepCartpole {
             System.out.print("To run this example, download and start the gym-http-api repo found at https://github.com/openai/gym-http-api.");
         }
 
-        //define the training
+        //학습 정의 
         AsyncNStepQLearningDiscreteDense<Box> dql = new AsyncNStepQLearningDiscreteDense<Box>(mdp, CARTPOLE_NET_NSTEP, CARTPOLE_NSTEP, manager);
 
-        //train
+        //학습
         dql.train();
 
-        //close the mdp (close connection)
+        //mdp 종료 (http 연결)
         mdp.close();
 
 
