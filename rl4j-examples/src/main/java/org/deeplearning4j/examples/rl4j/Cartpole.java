5487eae24b6fba9091ce8e935275819f1c811672
DaleePark <39670191+DaleePark@users.noreply.github.com>
2018-08-05 16:04:23 +0900

    Update Cartpole.java
    convert_kor

--- rl4j-examples/src/main/java/org/deeplearning4j/examples/rl4j/Cartpole.java
+++ rl4j-examples/src/main/java/org/deeplearning4j/examples/rl4j/Cartpole.java
@@ -16,7 +16,7 @@ import java.util.logging.Logger;
 /**
  * @author rubenfiszel (ruben.fiszel@epfl.ch) on 8/11/16.
  *
- * Main example for Cartpole DQN
+ * Cartpole DQN 메인 예제
  *
  * **/
 public class Cartpole
@@ -24,27 +24,27 @@ public class Cartpole
 
     public static QLearning.QLConfiguration CARTPOLE_QL =
             new QLearning.QLConfiguration(
-                    123,    //Random seed
-                    200,    //Max step By epoch
-                    150000, //Max step
-                    150000, //Max size of experience replay
-                    32,     //size of batches
-                    500,    //target update (hard)
-                    10,     //num step noop warmup
-                    0.01,   //reward scaling
+                    123,    //랜덤 시드
+                    200,    //에포크 최대 스텝
+                    150000, //최대 스텝
+                    150000, //반복 수행 최대 크기
+                    32,     //배치 크기
+                    500,    //타겟 업데이트 (hard)
+                    10,     //noop 시작 스텝 수
+                    0.01,   //보상 스케일링
                     0.99,   //gamma
-                    1.0,    //td-error clipping
-                    0.1f,   //min epsilon
-                    1000,   //num step for eps greedy anneal
-                    true    //double DQN
+                    1.0,    //td-error 클립핑
+                    0.1f,   //최소 입실론
+                    1000,   //입실론 탐욕 담금질(eps greedy anneal)을 위한 스텝 수
+                    true    //이중 DQN
             );
 
     public static DQNFactoryStdDense.Configuration CARTPOLE_NET =
             new DQNFactoryStdDense.Configuration(
-                    3,         //number of layers
-                    16,        //number of hidden nodes
-                    0.001,     //learning rate
-                    0.00       //l2 regularization
+                    3,         //계층 수
+                    16,        //은닉 노드 수
+                    0.001,     //학습률
+                    0.00       //l2 정규화
             );
 
     public static void main( String[] args )
@@ -55,29 +55,29 @@ public class Cartpole
 
     public static void cartPole() {
 
-        //record the training data in rl4j-data in a new folder (save)
+        //새 폴더의 rl4j-data에 학습 데이터 기록 (저장)
         DataManager manager = new DataManager(true);
 
-        //define the mdp from gym (name, render)
+        ///gym (name, render)을 이용해서 mdp 정의 
         GymEnv<Box, Integer, DiscreteSpace> mdp = null;
         try {
             mdp = new GymEnv("CartPole-v0", false, false);
         } catch (RuntimeException e){
             System.out.print("To run this example, download and start the gym-http-api repo found at https://github.com/openai/gym-http-api.");
         }
-        //define the training
+        //학습 정의 
         QLearningDiscreteDense<Box> dql = new QLearningDiscreteDense(mdp, CARTPOLE_NET, CARTPOLE_QL, manager);
 
-        //train
+        //학습
         dql.train();
 
-        //get the final policy
+        //최종 정책  
         DQNPolicy<Box> pol = dql.getPolicy();
 
-        //serialize and save (serialization showcase, but not required)
+        //직렬화와 저장 (직렬화는 필수는 아님)
         pol.save("/tmp/pol1");
 
-        //close the mdp (close http)
+        //mdp 종료 (http 연결)
         mdp.close();
 
 
@@ -86,15 +86,15 @@ public class Cartpole
 
     public static void loadCartpole(){
 
-        //showcase serialization by using the trained agent on a new similar mdp (but render it this time)
+        //유사한 새로운 mdp에서 학습된 에이전트를 이용하여 직렬화를 보여줌(렌더링은 이번에 수행)  
 
-        //define the mdp from gym (name, render)
+        //gym (name, render)을 이용해서 mdp 정의 
         GymEnv mdp2 = new GymEnv("CartPole-v0", true, false);
 
-        //load the previous agent
+        //이전 에이전트 로드 
         DQNPolicy<Box> pol2 = DQNPolicy.load("/tmp/pol1");
 
-        //evaluate the agent
+        //에이전트 평가 
         double rewards = 0;
         for (int i = 0; i < 1000; i++) {
             mdp2.reset();
