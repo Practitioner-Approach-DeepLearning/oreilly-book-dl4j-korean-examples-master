08944b0eb426c8f7648cd034d0e43c653134f4a0
DaleePark <39670191+DaleePark@users.noreply.github.com>
2018-08-05 12:53:05 +0900

    Update runexamples.sh
    convert kor

--- runexamples.sh
+++ runexamples.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-# Run one or all of the examples.
-# Use runexamples.sh -h to see the options.
+# 하나씩 또는 전체 예제 실행.
+# runexamples.sh -h 를 통해 가능한 옵션 확인.
+#   -h|--help          도움말을 보여주고 종료
+#   -a|--all           모든 예제 실행. 기본값은 실행할 예제 번호 입력을 대기함. 
+#   -n|--no-pauses     예제 사이에 대기하지 않음.(--all 옵션과 함께 사용)
 
 help() {
   [ -n "$1" ] && echo $1
@@ -14,7 +17,10 @@ where:
 EOF1
 }
 
-# This is shown only if all examples are executed.
+# # 모든 예제를 실행하는 경우에만 표시됨.
+#   각 예제가 실행되고 나면 몇몇 예제는 데이터 플롯이 포함 된 다이얼로그 팝업이 나타날
+#   수 있다. 데이터 플롯 애플리케이션을 종료하고 다음 예제를 실행하면 된다. 
+#   각 예제가 실행된 후에는 일시정지 된다. <return>을 눌러서 계속하거나 <ctrl-c>를 눌러서 종료하라.
 banner() {
   if [ $all -eq 0 ]
   then
@@ -66,25 +72,23 @@ do
   shift
 done
 
-# Most have class names that end with "Example", but not all.
-# So, we use a hack; we search the Java files for "void main"
-# to find all of them.
+# 대부분의 클래스 이름이 "Example"로 끝나지만 모두 그런것은 아니다. 
+# Java 파일 중에서 "void main"을 찾으면 모든 클래스를 찾을 수 있다. 
 
 dir=$PWD
 cd dl4j-examples/src/main/java
 
 find_examples() {
-  # Find all the Java files with "main" routines, then replace
-  # all '/' with '.', then remove extraneous leading '.' and
-  # the file extension, yielding the fully-qualified class name.
+  # "main"을 검색해서 모든 Java 파일을 찾은 다음 '/'를 '.'으로 변경.
+  # '.' 이후의 불필요한 공란 삭제 후 .java 확장자를 가지는 정규화된 클래스명 생성.
   find . -name '*.java' -exec grep -l 'void main' {} \; | \
     sed "s?/?.?g" | sed "s?^\.*\(.*\)\.java?\1?"
 }
 
-# The following works because IFS, the "field" separator is \n.
-# So, substituting the result of find_examples into the the
-# string and then evaluating the array definition, produces
-# an array of the class names!
+
+# 필드 구분기호는 \n.
+# find_examples()의 결과를 string으로 변경하고 arr에 채우면 
+# 클래스명 리스트인 arr가 생성 됨. 
 eval "arr=($(find_examples))"
 
 cd $dir
@@ -102,7 +106,7 @@ let which_one=0
 if [ $all -ne 0 ]
 then
 
-  for index in "${!arr[@]}"   # ! returns indices instead
+  for index in "${!arr[@]}"   # arr의 index 반환
   do
     let i=$index+1
     echo "[$(printf "%2d" $i)] ${arr[$index]}"
@@ -112,7 +116,7 @@ then
   if [ -z "$which_one" ]
   then
     which_one=0
-  elif [ $which_one = 'q' ]  # accept 'q' as "quit".
+  elif [ $which_one = 'q' ]  # 'q'를 종료('quit')로 간주
   then
     exit 0
   elif [ $which_one -le 0 -o $which_one -gt ${#arr[@]} ]
@@ -129,7 +133,7 @@ else
 
   banner
 
-  ## now loop through the above array
+  ## arr 내에서 반복 
   for e in "${arr[@]}"
   do
     runit "$e"
