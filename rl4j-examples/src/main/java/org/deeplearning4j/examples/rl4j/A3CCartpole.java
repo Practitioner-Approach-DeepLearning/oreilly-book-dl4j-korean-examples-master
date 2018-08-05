8fd184655e0a801c6bcd9a108d59cd2c62ca720b
DaleePark <39670191+DaleePark@users.noreply.github.com>
2018-08-05 15:49:18 +0900

    Update A3CCartpole.java
    convert_kor

--- rl4j-examples/src/main/java/org/deeplearning4j/examples/rl4j/A3CCartpole.java
+++ rl4j-examples/src/main/java/org/deeplearning4j/examples/rl4j/A3CCartpole.java
@@ -11,31 +11,31 @@ import org.deeplearning4j.rl4j.util.DataManager;
 /**
  * @author rubenfiszel (ruben.fiszel@epfl.ch) on 8/18/16.
  *
- * main example for A3C on cartpole
+ * A3C를 이용한 cartpole 문제 예제 
  *
  */
 public class A3CCartpole {
 
     private static A3CDiscrete.A3CConfiguration CARTPOLE_A3C =
             new A3CDiscrete.A3CConfiguration(
-                    123,            //Random seed
-                    200,            //Max step By epoch
-                    500000,         //Max step
-                    16,              //Number of threads
+                    123,            //랜덤 시드
+                    200,            //에포크 최대 스텝 
+                    500000,         //최대 스텝 
+                    16,              //쓰레드 수
                     5,              //t_max
-                    10,             //num step noop warmup
-                    0.01,           //reward scaling
+                    10,             // noop 스텝 수
+                    0.01,           //보상 스케일링
                     0.99,           //gamma
-                    10.0           //td-error clipping
+                    10.0           //td-error 클립핑
             );
 
 
 
     private static final ActorCriticFactorySeparateStdDense.Configuration CARTPOLE_NET_A3C = new ActorCriticFactorySeparateStdDense.Configuration(
-            3,                      //number of layers
-            16,                     //number of hidden nodes
-            0.001,                 //learning rate
-            0.000                   //l2 regularization
+            3,                      //계층 수
+            16,                     //은닉 노드 수
+            0.001,                 //학습률
+            0.000                   //l2 정규화 
     );
 
 
@@ -46,10 +46,10 @@ public class A3CCartpole {
 
     public static void A3CcartPole() {
 
-        //record the training data in rl4j-data in a new folder
+        //새 폴더의 rl4j-data에 학습 데이터 기록
         DataManager manager = new DataManager(true);
 
-        //define the mdp from gym (name, render)
+        //gym (name, render)을 이용해서 mdp 정의 
         GymEnv mdp = null;
         try {
         mdp = new GymEnv("CartPole-v0", false, false);
@@ -57,13 +57,13 @@ public class A3CCartpole {
             System.out.print("To run this example, download and start the gym-http-api repo found at https://github.com/openai/gym-http-api.");
         }
 
-        //define the training
+        //학습 정의 
         A3CDiscreteDense<Box> dql = new A3CDiscreteDense<Box>(mdp, CARTPOLE_NET_A3C, CARTPOLE_A3C, manager);
 
-        //start the training
+        //학습 시작
         dql.train();
 
-        //close the mdp (http connection)
+        //mdp 종료 (http 연결)
         mdp.close();
 
     }
