54176bf85aa61a1e1fdbcaaad173f1eed93c05bd
DaleePark <39670191+DaleePark@users.noreply.github.com>
2018-08-05 16:10:46 +0900

    Update Toy.java
    convert_kor

--- rl4j-examples/src/main/java/org/deeplearning4j/examples/rl4j/Toy.java
+++ rl4j-examples/src/main/java/org/deeplearning4j/examples/rl4j/Toy.java
@@ -19,7 +19,7 @@ import org.deeplearning4j.rl4j.util.DataManager;
 /**
  * @author rubenfiszel (ruben.fiszel@epfl.ch) on 8/11/16.
  *
- * main example for toy DQN
+ * 토이 DQN 예제
  *
  */
 public class Toy {
@@ -27,44 +27,44 @@ public class Toy {
 
     public static QLearning.QLConfiguration TOY_QL =
             new QLearning.QLConfiguration(
-                    123,   //Random seed
-                    100000,//Max step By epoch
-                    80000, //Max step
-                    10000, //Max size of experience replay
-                    32,    //size of batches
-                    100,   //target update (hard)
-                    0,     //num step noop warmup
-                    0.05,  //reward scaling
+                    123,   //랜덤 시드
+                    100000,//에포크 최대 스텝 
+                    80000, //최대 스텝
+                    10000, //반복 수행 최대 크기
+                    32,    //배치 크기
+                    100,   //타겟 업데이트 (hard)
+                    0,     //noop 시작 스텝 수
+                    0.05,  //보상 스케일링
                     0.99,  //gamma
-                    10.0,  //td-error clipping
-                    0.1f,  //min epsilon
-                    2000,  //num step for eps greedy anneal
-                    true   //double DQN
+                    10.0,  //td-error 클립핑
+                    0.1f,  //최소 입실론
+                    2000,  //입실론 탐욕 담금질(eps greedy anneal)을 위한 스텝 수
+                    true   //이중 DQN
             );
 
 
     public static AsyncNStepQLearningDiscrete.AsyncNStepQLConfiguration TOY_ASYNC_QL =
             new AsyncNStepQLearningDiscrete.AsyncNStepQLConfiguration(
-                    123,        //Random seed
-                    100000,     //Max step By epoch
-                    80000,      //Max step
-                    8,          //Number of threads
+                    123,        //랜덤 시드
+                    100000,     //에포크 최대 스텝
+                    80000,      //최대 스텝
+                    8,          //쓰레드 수
                     5,          //t_max
-                    100,        //target update (hard)
-                    0,          //num step noop warmup
-                    0.1,        //reward scaling
+                    100,        //타겟 업데이트 (hard)
+                    0,          //noop 시작 스텝 수
+                    0.1,        //보상 스케일링
                     0.99,       //gamma
-                    10.0,       //td-error clipping
-                    0.1f,       //min epsilon
-                    2000        //num step for eps greedy anneal
+                    10.0,       //td-error 클립핑
+                    0.1f,       //최소 입실론
+                    2000        //입실론 탐욕 담금질(eps greedy anneal)을 위한 스텝 수
             );
 
     public static DQNFactoryStdDense.Configuration TOY_NET =
             new DQNFactoryStdDense.Configuration(
-                    3,        //number of layers
-                    16,       //number of hidden nodes
-                    0.001,    //learning rate
-                    0.01      //l2 regularization
+                    3,        //계층 수
+                    16,       //은닉 노드 수
+                    0.001,    //학습률
+                    0.01      //l2 정규화
             );
 
     public static void main(String[] args )
@@ -76,41 +76,41 @@ public class Toy {
 
     public static void simpleToy() {
 
-        //record the training data in rl4j-data in a new folder
+        //새 폴더의 rl4j-data에 학습 데이터 기록.
         DataManager manager = new DataManager();
 
-        //define the mdp from toy (toy length)
+        //toy (toy length)로 부터 mdp 정의.
         SimpleToy mdp = new SimpleToy(20);
 
-        //define the training method
+        //학습 메소드 정의.
         Learning<SimpleToyState, Integer, DiscreteSpace, IDQN> dql = new QLearningDiscreteDense<SimpleToyState>(mdp, TOY_NET, TOY_QL, manager);
 
-        //enable some logging for debug purposes on toy mdp
+        //toy mdp에서 디버깅 목적으로 로깅 사용 가능.
         mdp.setFetchable(dql);
 
-        //start the training
+        //학습 시작.
         dql.train();
 
-        //useless on toy but good practice!
+        //토이 예제에서는 필요 없지만 연습용.
         mdp.close();
 
     }
 
     public static void hardToy() {
 
-        //record the training data in rl4j-data in a new folder
+        //새 폴더의 rl4j-data에 학습 데이터 기록 (저장)
         DataManager manager = new DataManager();
 
-        //define the mdp from toy (toy length)
+        //toy (toy length)로 부터 mdp 정의.
         MDP mdp = new HardDeteministicToy();
 
-        //define the training
+        //학습 정의.
         ILearning<SimpleToyState, Integer, DiscreteSpace> dql = new QLearningDiscreteDense(mdp, TOY_NET, TOY_QL, manager);
 
-        //start the training
+        //학습 시작.
         dql.train();
 
-        //useless on toy but good practice!
+        //토이 예제에서는 필요 없지만 연습용.
         mdp.close();
 
 
@@ -119,22 +119,22 @@ public class Toy {
 
     public static void toyAsyncNstep() {
 
-        //record the training data in rl4j-data in a new folder
+        //새 폴더의 rl4j-data에 학습 데이터 기록 (저장)
         DataManager manager = new DataManager();
 
-        //define the mdp
+        //mdp 정의.
         SimpleToy mdp = new SimpleToy(20);
 
-        //define the training
+        //학습 정의. 
         AsyncNStepQLearningDiscreteDense dql = new AsyncNStepQLearningDiscreteDense<SimpleToyState>(mdp, TOY_NET, TOY_ASYNC_QL, manager);
 
-        //enable some logging for debug purposes on toy mdp
+        //toy mdp에서 디버깅 목적으로 로깅 사용 가능.
         mdp.setFetchable(dql);
 
-        //start the training
+        //학습 시작.
         dql.train();
 
-        //useless on toy but good practice!
+        //토이 예제에서는 필요 없지만 연습용.
         mdp.close();
 
     }
